8d3ea0af7f77c184cb451f4ef5ee3b56e4119ce5
Deven Phillips <deven.phillips@gmail.com>
2018-10-29 13:51:03 -0400

    Stashing changes

--- src/test/java/gov/dhs/nppd/csc/nsd/yellowdog/RunCukesTest.java
+++ src/test/java/gov/dhs/nppd/csc/nsd/yellowdog/RunCukesTest.java
@@ -1,11 +1,19 @@
 package gov.dhs.nppd.csc.nsd.yellowdog;
 
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = "src/test/resources/features")
 public class RunCukesTest {
+
+  @BeforeClass
+  public static void initWebDriver() {
+    System.out.println("**### - ChromeDriver setup");
+		WebDriverManager.chromedriver().setup();
+  }
 }
