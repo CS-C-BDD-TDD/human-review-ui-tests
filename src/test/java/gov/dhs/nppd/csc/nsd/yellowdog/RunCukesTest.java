a5724711a60628f270f575f6dfca45960533d54f
Deven Phillips <deven.phillips@gmail.com>
2018-11-07 07:08:55 -0500

    Revert back to original configuration

--- src/test/java/gov/dhs/nppd/csc/nsd/yellowdog/RunCukesTest.java
+++ src/test/java/gov/dhs/nppd/csc/nsd/yellowdog/RunCukesTest.java
@@ -1,6 +1,5 @@
 package gov.dhs.nppd.csc.nsd.yellowdog;
 
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
@@ -9,9 +8,4 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = "src/test/resources/features")
 public class RunCukesTest {
-
-  @BeforeClass
-  public static void initWebDriver() {
-    System.out.println("**### - ChromeDriver setup");
-  }
 }
