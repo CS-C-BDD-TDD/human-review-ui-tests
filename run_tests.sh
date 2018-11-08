c0757b247328243afa2bc23bdb8684baf2478e8c
Deven Phillips <deven.phillips@gmail.com>
2018-11-08 08:19:15 -0500

    Wrap maven command tro address escaping issues

---
+++ run_tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+mvn \
+    -Dhr.restapi.url=http://human-review-backend-labs-test.apps.domino.rht-labs.com/api/v1 \
+    -Dhr.website.url=http://vue-app-labs-test.apps.domino.rht-labs.com/ \
+    -Dhr.regular.username=User1 \
+    -Dhr.regular.password=Pass1 \
+    -Dtest=RunCukesTest \
+    -Dwebdriver.timeouts.implicitlywait=10000 \
+    -Dcukes.config.file=config.properties \
+    -Dwebdriver.remote.driver=chrome \
+    -Dchrome.switches=\"--proxy-server=http://$1:8080\" \
+    -Dwebdriver.remote.url=http://zalenium:zalenium1234zalenium-labs-ci-cd.apps.domino.rht-labs.com/wd/hub \
+    clean test
